8690a74f6808ab5bbbf7f5f13201241a24071489
aoirint <aoirint@gmail.com>
2021-02-27 19:45:26 +0900

    wip; Tab Video streaming with WebRTC

--- app/scripts/background.ts
+++ app/scripts/background.ts
@@ -14,14 +14,12 @@ chrome.runtime.onInstalled.addListener((details) => {
 chrome.browserAction.onClicked.addListener((activeTab) => {
   chrome.tabCapture.capture({ audio: false, video: true }, (stream: MediaStream) => {
     const container: VideoStreamContainer = {
+      // TODO: contains tab title etc.
       stream: stream
     }
-
     videoStreams.push(container)
 
-    console.log(videoStreams)
-
-    if (videoStreams.length >= 3) {
+    if (videoStreams.length >= 1) {
       chrome.tabs.create({ url: chrome.extension.getURL('pages/index.html') })
     }
   })
@@ -31,12 +29,47 @@ chrome.browserAction.setBadgeText({
   text: '\'Allo'
 })
 
-chrome.runtime.onMessage.addListener((request: VideoStreamRequest, sender, sendResponse) => {
+chrome.runtime.onMessage.addListener((request: VideoStreamRequest, sender: chrome.runtime.MessageSender, sendResponse) => {
   if (request.type === 'video') {
-    const response: VideoStreamResponse = {
-      videoStreams: videoStreams
+    if (! sender.tab) { console.error('sender.tab cannot be undefined. it\'s bug,'); return }
+    let senderTab: chrome.tabs.Tab = sender.tab
+
+    if (! senderTab.id) { console.error('sender.tab.id cannot be undefined. it\'s bug,'); return }
+    let senderTabId: number = senderTab.id
+
+    const pc = new RTCPeerConnection({iceServers:[]})
+
+    for (let videoStream of videoStreams) {
+      const stream: MediaStream = videoStream.stream
+      const tracks: MediaStreamTrack[] = stream.getTracks()
+      console.log(stream)
+      console.log(tracks)
+
+      const track: MediaStreamTrack = stream.getTracks()[0]
+
+      pc.addTrack(track, stream)
     }
-    sendResponse(response)
+
+    pc.createOffer().then((offer: RTCSessionDescriptionInit) => {
+      pc.setLocalDescription(offer).then(() => {
+        let connectInfo: chrome.tabs.ConnectInfo = {
+          name: 'tabCaptureConnection',
+        }
+
+        let port = chrome.tabs.connect(senderTabId, connectInfo)
+        port.onDisconnect.addListener((port: chrome.runtime.Port) => {
+        })
+
+        port.onMessage.addListener((message: any, port: chrome.runtime.Port) => {
+          let sdp: RTCSessionDescription = <RTCSessionDescription> message
+
+          pc.setRemoteDescription(sdp)
+        })
+
+        port.postMessage(pc.localDescription)
+      })
+    })
+
   }
 
   return true
--- app/scripts/index.ts
+++ app/scripts/index.ts
@@ -7,12 +7,37 @@ import { VideoStreamResponse } from './interfaces/VideoStreamResponse'
 
 console.log('Index page opened!')
 
+
+chrome.runtime.onConnect.addListener((port: chrome.runtime.Port) => {
+  if (port.name === 'tabCaptureConnection') {
+    port.onMessage.addListener((message: any, port: chrome.runtime.Port) => {
+      let pc = new RTCPeerConnection({iceServers:[]})
+      pc.ontrack = (event: RTCTrackEvent) => {
+        console.log(event)
+
+        let videoElement = <HTMLVideoElement> document.createElement('video')
+        videoElement.style.backgroundColor = 'gray';
+        const streams: readonly MediaStream[] = event.streams
+
+        videoElement.srcObject = streams[0]
+        console.log(videoElement)
+        
+        document.body.appendChild(videoElement)
+      };
+
+      let sdp: RTCSessionDescription = <RTCSessionDescription> message
+      pc.setRemoteDescription(sdp).then(() => {
+        pc.createAnswer().then((answer: RTCSessionDescriptionInit) => {
+          pc.setLocalDescription(answer).then(() => {
+            port.postMessage(pc.localDescription)
+          })
+        })
+      })
+    })
+  }
+})
+
 const request: VideoStreamRequest = {
   type: 'video'
 }
-
-chrome.runtime.sendMessage(request, (response: VideoStreamResponse) => {
-  const containers: VideoStreamContainer[] = response.videoStreams
-
-  console.log(containers)
-})
+chrome.runtime.sendMessage(request)
