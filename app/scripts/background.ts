b4666dde6d1cbebb5896e1b00f1dd0cd805545db
manabe5260 <45477915+manabe5260@users.noreply.github.com>
2021-02-27 13:28:25 +0900

    issue8_wip
    issue8 2 error

--- app/scripts/background.ts
+++ app/scripts/background.ts
@@ -1,6 +1,10 @@
 // Enable chromereload by uncommenting this line:
 // import 'chromereload/devonly'
 
+let audioContext: AudioContext
+let streamSource: MediaStreamAudioSourceNode
+let gainNode: GainNode
+
 chrome.runtime.onInstalled.addListener((details) => {
   console.log('previousVersion', details.previousVersion)
 })
@@ -11,6 +15,21 @@ chrome.browserAction.onClicked.addListener((activeTab) => {
 
 chrome.browserAction.setBadgeText({
   text: '\'Allo'
+})
+
+chrome.tabCapture.capture({ audio: true, video: false }, (stream: MediaStream) => {
+  console.log("typeof")
+  console.log(typeof(audioContext))
+  console.log(typeof(streamSource))
+
+  audioContext = new AudioContext()
+  streamSource = audioContext.createMediaStreamSource(stream)
+  gainNode = audioContext.createGain()
+
+  streamSource.connect(gainNode)
+  gainNode.connect(audioContext.destination)
+
+  gainNode.gain.value = 1.0
 })
 
 console.log('\'Allo \'Allo! Event Page for Browser Action')
