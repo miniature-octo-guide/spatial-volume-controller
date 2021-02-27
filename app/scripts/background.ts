cf7e5b7356cddf53ac3c86e50f05ca2fea4f2d68
RyotaYamamoto990319 <ryou.ta.ya.03@gmail.com>
2021-02-27 14:57:12 +0900

    add videoStream

--- app/scripts/background.ts
+++ app/scripts/background.ts
@@ -1,12 +1,16 @@
 // Enable chromereload by uncommenting this line:
 // import 'chromereload/devonly'
 
+let objectUrl : DOMString
+
 chrome.runtime.onInstalled.addListener((details) => {
   console.log('previousVersion', details.previousVersion)
 })
 
 chrome.browserAction.onClicked.addListener((activeTab) => {
-  chrome.tabs.create({ url: chrome.extension.getURL('pages/index.html') })
+  chrome.tabCapture.capture({ audio: false, video: true }, (stream: MediaStream) => {
+    objectUrl = URL.createObjectURL(stream);
+  }
 })
 
 chrome.browserAction.setBadgeText({
