793333833916933edec9c2ab22d5b3f78bd1f5a5
nenenekko <36717502+nenenekko@users.noreply.github.com>
2021-02-28 13:44:20 +0900

    画面上でHTML要素を動かすUIの作成 (#17)
    * くらすで座標管理
    
    * デザインページ上で動作
    
    * remove unnecessary scripts
    
    * remove 3rd-party resources & big refactoring
    
    * refactoring: lint-fix
    
    * added getSpeakerBox & refactoring
    
    Co-authored-by: aoirint <aoirint@gmail.com>

--- app/scripts/index.ts
+++ app/scripts/index.ts
@@ -1,4 +1,178 @@
-// Enable chromereload by uncommenting this line:
-// import 'chromereload/devonly'
+// import { v4 as uuidv4 } from 'uuid'
+import { SpeakerBox } from './interfaces/SpeakerBox'
 
-console.log('Index page opened!')
+let dragStartX: number = 0
+let dragStartY: number = 0
+
+function getSpeakerBoxes (): SpeakerBox[] {
+  const speakerBoxDomList: NodeList = document.querySelectorAll('.speaker-box')
+  const ret: SpeakerBox[] = []
+
+  for (let index = 0; index < speakerBoxDomList.length; index++) {
+    const dom: Node = speakerBoxDomList[index]
+    if (!(dom instanceof HTMLElement)) continue
+
+    const rect = dom.getBoundingClientRect()
+    const x: number = (rect.left + rect.right) / 2
+    const y: number = (rect.top + rect.bottom) / 2
+
+    const id: string | null = dom.dataset.id ?? null
+    const text: string | null = dom.dataset.text ?? null
+
+    const box: SpeakerBox = {
+      x: x,
+      y: y,
+      id: id,
+      text: text
+    }
+
+    ret.push(box)
+  }
+
+  return ret
+}
+
+function initMain (): void {
+  document.body.addEventListener('mousemove', _onMouseMove, false) // 3rd == false: bottom-up propagation
+  document.body.addEventListener('touchmove', _onMouseMove, false)
+  // document.body.addEventListener("mouseleave", _onMouseUp, false)
+  // document.body.addEventListener("touchleave", _onMouseUp, false)
+
+  const bodyRect = document.body.getBoundingClientRect()
+  const centerX = (bodyRect.left + bodyRect.right) / 2
+  const centerY = (bodyRect.top + bodyRect.bottom) / 2
+
+  const listenerIcon = _createListenerIcon(centerX, centerY)
+  document.body.appendChild(listenerIcon)
+
+  const speakerIcon = _createSpeakerIcon(300, 100, 'my-unique-id', 'Room X')
+  document.body.appendChild(speakerIcon)
+  // icons.push(new SpeakerIcon(100+i*100,100+i*100, "Room" + i));
+
+  const speakers = getSpeakerBoxes()
+  console.log(speakers)
+}
+
+// TODO: set center point
+function _createListenerIcon (left: number, top: number): HTMLElement {
+  // TODO: replace with React or Vue
+  const dom: HTMLDivElement = document.createElement('div')
+  dom.classList.add('listener-box')
+  dom.classList.add('drag-and-drop')
+
+  dom.style.left = `${left}px`
+  dom.style.top = `${top}px`
+
+  const img = document.createElement('img')
+  img.src = '../images/yamanekko.jpg'
+  img.id = 'my-icon'
+  dom.appendChild(img)
+
+  dom.addEventListener('mousedown', _onIconMouseDown, false)
+  dom.addEventListener('touchstart', _onIconMouseDown, false)
+
+  // マウスボタンが離されたとき、またはカーソルが外れたとき発火
+  dom.addEventListener('mouseup', _onMouseUp, false)
+  dom.addEventListener('touchend', _onMouseUp, false)
+
+  return dom
+}
+
+function _createSpeakerIcon (left: number, top: number, id: string, text: string): HTMLElement {
+  const dom: HTMLDivElement = document.createElement('div')
+  dom.classList.add('speaker-box')
+  dom.classList.add('drag-and-drop')
+
+  dom.style.left = `${left}px`
+  dom.style.top = `${top}px`
+
+  dom.dataset.id = id
+  dom.dataset.text = text
+
+  const domP: HTMLParagraphElement = document.createElement('p')
+  domP.innerText = text
+  dom.appendChild(domP)
+
+  dom.addEventListener('mousedown', _onIconMouseDown, false)
+  dom.addEventListener('touchstart', _onIconMouseDown, false)
+
+  // マウスボタンが離されたとき、またはカーソルが外れたとき発火
+  dom.addEventListener('mouseup', _onMouseUp, false)
+  dom.addEventListener('touchend', _onMouseUp, false)
+
+  return dom
+}
+
+interface PageLocation {
+  pageX: number
+  pageY: number
+}
+
+function _onIconMouseDown (e: MouseEvent | TouchEvent): void {
+  const target: EventTarget | null = e.currentTarget
+  if (!(target instanceof HTMLElement)) {
+    console.error('event target is not HTML element')
+    return
+  }
+
+  // タッチイベントとマウスのイベントの差異を吸収
+  let event: PageLocation
+  if (e instanceof TouchEvent) {
+    event = e.changedTouches[0]
+  } else {
+    event = e
+  }
+
+  // クラスに .drag を追加
+  target.classList.add('drag')
+
+  // 要素内の相対座標を取得
+  dragStartX = event.pageX - this.offsetLeft
+  dragStartY = event.pageY - this.offsetTop
+
+  console.log(dragStartX, dragStartY)
+}
+
+// マウスカーソルが動いたときに発火
+function _onMouseMove (e: MouseEvent | TouchEvent): void {
+  // フリックしたときに画面を動かさないようにデフォルト動作を抑制
+  e.preventDefault()
+
+  // タッチイベントとマウスのイベントの差異を吸収
+  let event: PageLocation
+  if (e instanceof TouchEvent) {
+    event = e.changedTouches[0]
+  } else {
+    event = e
+  }
+
+  const drag: HTMLElement | null = document.querySelector('.drag')
+  if (drag == null) return
+
+  // const rect: Element = drag.getBoundingClientRect()
+  // const centerX = (rect.left + rect.right) / 2
+  // const centerY = (rect.top + rect.bottom) / 2
+
+  // マウスが動いた場所に要素を動かす
+  const newX: number = event.pageX - dragStartX
+  const newY: number = event.pageY - dragStartY
+
+  drag.style.left = `${newX}px`
+  drag.style.top = `${newY}px`
+}
+
+// マウスボタンが上がったら発火
+function _onMouseUp (e: MouseEvent | TouchEvent): void {
+  const drag: HTMLElement | null = document.querySelector('.drag') as HTMLElement
+  if (drag == null) {
+    console.error('drag object is null')
+    return
+  }
+
+  // クラス .drag を消す
+  drag.classList.remove('drag')
+}
+
+window.onload = () => {
+  initMain()
+}
---
+++ app/scripts/popup.ts
@@ -0,0 +1,31 @@
+// Enable chromereload by uncommenting this line:
+// import 'chromereload/devonly'
+
+console.log('Popup page opened!')
+
+function initPopup (): void {
+  const elmOpen: HTMLAnchorElement | null = document.querySelector('#id_open') as HTMLAnchorElement
+  if (elmOpen == null) return
+  elmOpen.addEventListener('click', () => {
+    chrome.tabs.create({ url: chrome.extension.getURL('pages/index.html') })
+  })
+
+  const elmTrack: HTMLAnchorElement | null = document.querySelector('#id_track') as HTMLAnchorElement
+  if (elmTrack == null) { console.error('track element not found'); return }
+  elmTrack.addEventListener('click', () => {
+    // TODO: track tab
+  })
+
+  const elmUntrack: HTMLAnchorElement | null = document.querySelector('#id_untrack') as HTMLAnchorElement
+  if (elmUntrack == null) { console.error('untrack element not found'); return }
+  elmUntrack.addEventListener('click', () => {
+    // TODO: untrack tab
+  })
+}
+
+window.onload = () => {
+  initPopup()
+}
+
+// TODO: make Untrack visible if current tab is tracked
+// TODO: make track invisible if current tab is untracked
